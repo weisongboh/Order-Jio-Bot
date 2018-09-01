479003cbb4dfa1bed8a0b191e465ffc8d125104b
weisongboh <teoweisong.94@gmail.com>
2018-09-01 20:39:10 +0800

    Initial Commit

---
+++ supper-bot.py
@@ -0,0 +1,10 @@
+import time as ostime
+import telepot
+from telepot.loop import *
+from telepot.namedtuple import *
+from datetime import *
+import pandas as pd
+import threading as thr
+from config import *
+
+#Telegram supper bot
